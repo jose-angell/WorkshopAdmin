0c4f290f2d436047b4e74d2042297a1b0071c265
jose Angel <josegallardo.cor@gmail.com>
2026-04-29 23:19:38 -0600

    feat: creacion de tabla  y stats para la pantalla de ordenes de servicio

---
+++ WorkshopAdmin.Shared/Dtos/ServiceOrders/ServiceOrderStatsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkshopAdmin.Shared.Dtos.ServiceOrders;
+
+public class ServiceOrderStatsDto
+{
+    public int ActiveOrders { get; set; }
+    public int NewOrdersThisWeek { get; set; }
+    public int PendingDiagnostics { get; set; }
+    public int AvgDiagnosticHours { get; set; }
+    public decimal TotalRepairCost { get; set; }
+}
