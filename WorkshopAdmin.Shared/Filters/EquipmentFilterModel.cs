0b2da5d0735adc67da94f2aa0aeae2da9ccf3462
jose Angel <josegallardo.cor@gmail.com>
2026-05-18 23:04:07 -0600

    feat: filtro para la trabla de equipment

---
+++ WorkshopAdmin.Shared/Filters/EquipmentFilterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkshopAdmin.Shared.Enums;
+
+namespace WorkshopAdmin.Shared.Filters
+{
+    public class EquipmentFilterModel
+    {
+        public string Search { get; set; } = string.Empty;
+
+        public bool? IsActive { get; set; }
+
+        public EquipmentType? Type { get; set; }
+    }
+}
