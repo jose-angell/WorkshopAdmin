2cd43d5318933ce6398c2258a7de45f2c0a1e4a3
jose Angel <josegallardo.cor@gmail.com>
2026-05-19 20:15:17 -0600

    feat: filtro para la tabla de inventario

---
+++ WorkshopAdmin.Shared/Filters/PartFilterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkshopAdmin.Shared.Enums;
+
+namespace WorkshopAdmin.Shared.Filters
+{
+    public class PartFilterModel
+    {
+        public string Search { get; set; } = string.Empty;
+
+        public bool? IsActive { get; set; }
+
+        public PartCategory? Category { get; set; }
+    }
+}
