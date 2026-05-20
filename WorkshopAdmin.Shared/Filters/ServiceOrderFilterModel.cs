0d3fa1899e56ed61207d3a4c3616e6a57a51a628
jose Angel <josegallardo.cor@gmail.com>
2026-05-19 23:55:58 -0600

    feat: filto para la tabla de ordenes de servicio

---
+++ WorkshopAdmin.Shared/Filters/ServiceOrderFilterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkshopAdmin.Shared.Enums;
+
+namespace WorkshopAdmin.Shared.Filters
+{
+    public class ServiceOrderFilterModel
+    {
+        public string Search { get; set; } = string.Empty;
+
+        public bool? IsActive { get; set; }
+
+        public ServiceOrderStatus? orderStatus { get; set; }
+        public ServiceType? serviceType { get; set; }
+    }
+}
